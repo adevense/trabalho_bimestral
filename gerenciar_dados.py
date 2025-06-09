4a8cc7ccc42c0d86d0c8f49bce3a0f3a65c4d73f
adevense <adevense016@gmail.com>
2025-06-09 18:01:38 -0300

    adicionar participantes parcialmente completo

--- gerenciar_dados.py
+++ gerenciar_dados.py
@@ -1,20 +1,14 @@
 import json
 
-def importar_dados_eventos():
-    with open('banco.json','r') as banco: # estou abrindo o arquivo json em modo de leitura(r)
+def importar_dados():
+    with open('banco.json','r', encoding='utf-8') as banco: # estou abrindo o arquivo json em modo de leitura(r)
         dados  = json.load(banco) # estou carregando o conteudo do arquivo json para um dicionario dados
     eventos = dados["eventos"] # estou pegando o conteudo do dicionario dados e atribuindo a variavel evento
-    return eventos
-
-def importar_dados_participantes():
-    with open('banco.json','r') as banco: 
-        dados  = json.load(banco) 
     participantes = dados["participantes"] 
-    return participantes
+    return eventos,participantes
 
-def salvar_dados():
-    importar_dados_eventos()
-    importar_dados_participantes()
-    with open('banco.json','w') as banco: # estou abrindo o arquivo json em modo de escrita(w)
-        json.dump({"eventos": importar_dados_eventos(), "participantes": importar_dados_participantes()}, banco, indent=4)
+
+def salvar_dados(eventos,participantes):
+    with open('banco.json','w', encoding='utf-8') as banco: # estou abrindo o arquivo json em modo de escrita(w)
+        json.dump({"eventos": eventos, "participantes": participantes}, banco, indent=4, ensure_ascii=False)
 
--- gerenciar_participantes.py
+++ gerenciar_participantes.py
@@ -1,5 +1,37 @@
+from gerenciar_dados import *
+
 def adicionar_participante():
-    pass
+    eventos,participantes = importar_dados()
+    nome = input("Digite o nome do participante: ")
+    while True:
+        cpf = input("Digite o CPF do participante no formato XXX.XXX.XXX-XX: ")
+        if len(cpf) == 14 and cpf[3] == '.' and cpf[7] == '.' and cpf[11] == '-':
+            if cpf in [p['cpf'] for p in participantes]:
+                print("Participante já cadastrado com este CPF.")
+                continuar = input("Deseja tentar novamente? (s/n): ").lower()
+                if continuar == 's':
+                    print('')
+                    
+                else:
+                    print("Cadastro cancelado.")
+                    return
+            else:
+                break
+        else:
+            print("CPF inválido. Certifique-se de que o formato está correto (XXX.XXX.XXX-XX) e tente novamente.")
+         
+
+        
+    email = input("Digite o email do participante: ")
+    novo_participante = {
+        "cpf": cpf,
+        "nome": nome,
+        "email": email,
+        "preferencias_tematicas":[]
+    }
+    
+    participantes.append(novo_participante)
+    salvar_dados(eventos,participantes)
 
 def remover_participante():
     pass
@@ -25,3 +57,4 @@ def listar_evento_por_participante():
 def buscar_remover_participante_duplicado_evento():
     pass
 
+adicionar_participante()
